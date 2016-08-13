40fb978645ce269f3d3a376c4d64eb549106b04e
James Larisch <james@jameslarisch.com>
2016-08-13 13:44:25 -0400

    initialize

---
+++ pybfilter.py
@@ -0,0 +1,46 @@
+import math
+import mmh3
+
+LN2SQUARED = math.pow(math.log(2), 2)
+LN2 = math.log(2)
+
+class BFilter:
+    def __init__(self, elements, fprate, ntweak = 0):
+        size = -1.0 / LN2SQUARED * elements * math.log(fprate)
+        filter_size = int(math.floor(size / 8.0))
+        self.vdata = bytearray(filter_size);
+        self.nhashfuncs = int(math.floor(filter_size * 8.0 / elements * LN2))
+        self.ntweak = ntweak
+
+    def hash(self, hashnum, data):
+        seed = hashnum #((hashnum * 0xFBA4C795) + self.ntweak) & 0xFFFFFFFF
+        hsh = mmh3.hash(data, seed)
+        if hsh < 0:
+            hsh = (1 << 32) + hsh
+        return hsh % (len(self.vdata) * 8)
+
+    def insert(self, data):
+        for i in range(0, self.nhashfuncs):
+            index = self.hash(i, data)
+            position = 1 << (7 & index)
+            print(index >> 3)
+            print(position)
+            print("--")
+            self.vdata[index >> 3] |= position
+        return self
+
+    def contains(self, data):
+        if (len(self.vdata) == 0):
+            return False
+        for i in range(0, self.nhashfuncs):
+            index = self.hash(i, data)
+            if (self.vdata[index >> 3] & (1 << (7 & index)) == 0):
+                return False
+        return True
+
+    def clear(self):
+        self.vdata = bytearray(len(self.vdata))
+
+
+
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pybfilter',
+      version='0.1',
+      description='A Python bloom filter.',
+      url='http://github.com/semaj/pybfilter',
+      author='James Larisch',
+      author_email='...',
+      license='MIT',
+      install_requires=[
+          'mmh3',
+      ],
+      zip_safe=False)
